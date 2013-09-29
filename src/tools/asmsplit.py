2335149b88def9590ab4a06bed73db564e804247
alexey.lysiuk <alexey.lysiuk@gmail.com>
2013-09-29 12:28:44 +0300

    + support for interleaving source files in asmsplit

--- src/tools/asmsplit.py
+++ src/tools/asmsplit.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
- 
+
 # ---------------------------------------------------------------------------
 #  OpenChasm - Free software reconstruction of Chasm: The Rift game
 #  Copyright (C) 2013 Alexey Lysiuk
-# 
+#
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
-# 
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-# 
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # ---------------------------------------------------------------------------
@@ -61,19 +61,21 @@ infile = open(sys.argv[1])
 currentfilename = ''
 outfile = None
 
+processedfiles = []
+
 for line in infile:
 	if re.match('cseg09\s+ends', line):
-		# have no interest in cseg10 (Pascal library functions) 
+		# have no interest in cseg10 (Pascal library functions)
 		# and following data segments
 		break
 	elif re.match('cseg\d{2}\s+ends', line):
 		currentfilename = ''
-		
+
 		finish(outfile)
 		outfile = None
 	elif line.startswith('; #line	"'):
 		filename = line.split('"')[1]
-		
+
 		if filename.endswith('TPSTRING.PAS'):
 			continue
 
@@ -82,12 +84,12 @@ for line in infile:
 
 			filename = filename.lower().replace('.pas', '.cpp')
 			pathparts = filename.split('\\')
-			
+
 			if len(pathparts) > 1:
 				filename = pathparts.pop()
 				path = os.sep.join(pathparts)
 				filename = path + os.sep + filename
-			
+
 				try:
 					os.makedirs(path)
 				except OSError:
@@ -95,9 +97,16 @@ for line in infile:
 
 			finish(outfile)
 
-			outfile = open(filename, 'w')
+			mode = 'w'
 
-			print('Writing ' + filename + '...')			
+			if filename in processedfiles:
+				mode = 'a'
+			else:
+				processedfiles.append(filename)
+
+			outfile = open(filename, mode)
+
+			print('Writing ' + filename + '...')
 			writeheader(outfile)
 
 	if outfile:
